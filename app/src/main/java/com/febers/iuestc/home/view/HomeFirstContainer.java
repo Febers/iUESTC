ed89d1b7dc97d0a2175f7a0b26016e64ddaf3483
Febers <febers418@gmail.com>
2018-09-27 18:04:22 +0800

    fixed bugs

---
+++ app/src/main/java/com/febers/iuestc/home/view/HomeFirstContainer.java
@@ -0,0 +1,31 @@
+/*
+ * Created by Febers 2018.
+ * Copyright (c). All rights reserved.
+ *
+ * Last Modified 18-9-4 下午6:17
+ *
+ */
+
+package com.febers.iuestc.home.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.febers.iuestc.R;
+import com.febers.iuestc.base.BaseFragment;
+import com.febers.iuestc.module.course.view.CourseFragment;
+
+public class HomeFirstContainer extends BaseFragment {
+    @Override
+    protected int setContentView() {
+        return R.layout.container_home_first;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (findChildFragment(CourseFragment.class) == null) {
+            loadRootFragment(R.id.first_container_layout, CourseFragment.newInstance(""));
+        }
+    }
+}
---
+++ app/src/main/java/com/febers/iuestc/home/view/HomeThirdContainer.java
@@ -0,0 +1,33 @@
+/*
+ * Created by Febers 2018.
+ * Copyright (c). All rights reserved.
+ *
+ * Last Modified 18-9-4 下午6:18
+ *
+ */
+
+package com.febers.iuestc.home.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.febers.iuestc.R;
+import com.febers.iuestc.base.BaseFragment;
+import com.febers.iuestc.module.more.MoreFragment;
+
+public class HomeThirdContainer extends BaseFragment {
+
+    @Override
+    protected int setContentView() {
+        return R.layout.container_home_third;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (findChildFragment(MoreFragment.class) == null) {
+            loadRootFragment(R.id.third_container_layout, MoreFragment.newInstance(""));
+        }
+    }
+}
